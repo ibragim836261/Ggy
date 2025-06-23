a66aae6cecf5f38bf9d2d806448d02e04497361a
ibragim836261 <ibragimmirzoyev667@gmail.com>
2025-06-23 17:41:06 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function confirmId() {
+    alert("Удостоверение подтверждено!");
+}
+
+function showId() {
+    alert("Удостоверение предъявлено сотруднику");
+}
+
+// Дополнительные функции по желанию:
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Удостоверение загружено");
+});
